6c683757d3cc2ea9568a7f9730360866ade98208
Umherirrender <umherirrender_de.wp@web.de>
2019-08-07 17:15:22 +0200

    Remove entry point check for files only contain classes
    See https://www.mediawiki.org/w/index.php?title=Register_globals&oldid=3063934#Make_sure_code_is_only_executed_in_the_right_context
    for more information
    
    Bug: T230026
    Change-Id: Ie55672e3f74fd879b7bf8b5f422ffe1406555ef7

--- SacredText.lookup.php
+++ SacredText.lookup.php
@@ -1,14 +1,5 @@
 <?php
-# Alert the user that this is not a valid entry point to
-# MediaWiki if they try to access the special pages file directly.
-if (!defined('MEDIAWIKI')) {
-	echo <<<EOT
-To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/SacredText/SacredText.setup.php" );
-EOT;
-	exit( 1 );
-}
- 
+
 class SacredTextLookup {
  
 	public static function parseInput( $input, &$book, &$chapternum, &$versenums,
